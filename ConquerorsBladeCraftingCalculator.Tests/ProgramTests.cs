ab82abfa92dd2a505e28c36ceabf6e8424bda8cf
Daniël knippenborg <danielknippenborg@gmail.com>
2025-10-27 01:58:03 +0000

    added test draft

---
+++ ConquerorsBladeCraftingCalculator.Tests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿namespace ConquerorsBladeCraftingCalculator.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            //Arrange
+            var example = true;
+
+            //Act
+            var result = Program.DoThing(example);
+
+            Assert.True(result);
+        }
+    }
+}
--- ConquerorsBladeCraftingCalculator/Program.cs
+++ ConquerorsBladeCraftingCalculator/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace ConquerorsBladeCraftingCalculator
+﻿
+namespace ConquerorsBladeCraftingCalculator
 {
     public class Program
     {
+        public static bool DoThing(bool example)
+        {
+            return true;
+        }
+
         public static void Main()
         {
 
